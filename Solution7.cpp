521e7d66c0b53103e7faa18b488cde93f513a537
Klistas <fukoyjh@gmail.com>
2022-06-14 10:16:38 +0900

    commit #3

--- Solution7.cpp
+++ Solution7.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 
 
+
+
+
+
+
+
+bool verticalBingo1 = false;
+bool verticalBingo2 = false;
+bool verticalBingo3 = false;
+bool verticalBingo4 = false;
+bool verticalBingo5 = false;
+
+bool horizontalBingo1 = false;
+bool horizontalBingo2 = false;
+bool horizontalBingo3 = false;
+bool horizontalBingo4 = false;
+bool horizontalBingo5 = false;
+
+bool diagonalBingo1 = false;
+bool diagonalBingo2 = false;
+
 int main()
 {
 	// 1. 빙고게임 판 생성.
@@ -30,10 +51,11 @@ int main()
 			isExist[boardNum] = true;
 		}
 	}
+		int bingoCount = 0;
 	while (1)
 	{
-		int bingoCount = 0;
-
+	
+		bool bingocheck = false;
 		for (int i = 0; i < 5; i++)
 		{
 			for (int j = 0; j < 5; j++)
@@ -70,10 +92,114 @@ int main()
 			}
 
 		}
-		system("cls");
+			for (int i = 0; i < 5; i++)
+				{
+				for (int j = 0; j < 5; j++)
+				{
+
+					// 빙고가 완성되면, 빙고 개수 count (중첩된 빙고개수 제외하는 것이 어려웠음. 리팩토링 필요.)
+					if (board[0][0] == NULL && board[1][1] == NULL && board[2][2] == NULL && board[3][3] == NULL && board[4][4] == NULL && diagonalBingo1 == false)
+					{
+						bingocheck = true;
+						diagonalBingo1 = true;
+
+					}
+					if (board[0][4] == NULL && board[1][3] == NULL && board[2][2] == NULL && board[3][1] == NULL && board[4][0] == NULL && diagonalBingo2 == false)
+					{
+						bingocheck = true;
+						diagonalBingo2 = true;
+
+					}
+					switch (i)
+					{
+					case 0:
+						if (board[i][0] == NULL && board[i][1] == NULL && board[i][2] == NULL && board[i][3] == NULL && board[i][4] == NULL && verticalBingo1 == false)
+						{
+							bingocheck = true;
+							verticalBingo1 = true;
+							break;
+						}
+					case 1:
+						if (board[i][0] == NULL && board[i][1] == NULL && board[i][2] == NULL && board[i][3] == NULL && board[i][4] == NULL && verticalBingo2 == false)
+						{
+							bingocheck = true;
+							verticalBingo2 = true;
+							break;
+						}
+					case 2:
+						if (board[i][0] == NULL && board[i][1] == NULL && board[i][2] == NULL && board[i][3] == NULL && board[i][4] == NULL && verticalBingo3 == false)
+						{
+							bingocheck = true;
+							verticalBingo3 = true;
+							break;
+						}
+					case 3:
+						if (board[i][0] == NULL && board[i][1] == NULL && board[i][2] == NULL && board[i][3] == NULL && board[i][4] == NULL && verticalBingo4 == false)
+						{
+							bingocheck = true;
+							verticalBingo4 = true;
+							break;
+						}
+					case 4:
+						if (board[i][0] == NULL && board[i][1] == NULL && board[i][2] == NULL && board[i][3] == NULL && board[i][4] == NULL && verticalBingo5 == false)
+						{
+							bingocheck = true;
+							verticalBingo5 = true;
+							break;
+						}
+					}
+
+					switch (j)
+					{
+					case 0:
+						if (board[0][j] == NULL && board[1][j] == NULL && board[2][j] == NULL && board[3][j] == NULL && board[4][j] == NULL && horizontalBingo1 == false)
+						{
+							bingocheck = true;
+							horizontalBingo1 = true;
+							break;
+						}
+					case 1:
+						if (board[0][j] == NULL && board[1][j] == NULL && board[2][j] == NULL && board[3][j] == NULL && board[4][j] == NULL && horizontalBingo2 == false)
+						{
+							bingocheck = true;
+							horizontalBingo2 = true;
+							break;
+						}
+					case 2:
+						if (board[0][j] == NULL && board[1][j] == NULL && board[2][j] == NULL && board[3][j] == NULL && board[4][j] == NULL && horizontalBingo3 == false)
+						{
+							bingocheck = true;
+							horizontalBingo3 = true;
+							break;
+						}
+					case 3:
+						if (board[0][j] == NULL && board[1][j] == NULL && board[2][j] == NULL && board[3][j] == NULL && board[4][j] == NULL && horizontalBingo4 == false)
+						{
+							bingocheck = true;
+							horizontalBingo4 = true;
+							break;
+						}
+					case 4:
+						if (board[0][j] == NULL && board[1][j] == NULL && board[2][j] == NULL && board[3][j] == NULL && board[4][j] == NULL && horizontalBingo5 == false)
+						{
+							bingocheck = true;
+							horizontalBingo5 = true;
+							break;
+						}
+					}
+				}
+			}
+			if (bingocheck == true)
+				bingoCount++;
+				system("cls");
 	}
 		// 5. 빙고 개수 확인.
-
+								
+								
+								
+					
+					
+				
 		// 6. 1번 부터 반복.
 
 	return 0;
--- Solution8.cpp
+++ Solution8.cpp
@@ -9,5 +9,61 @@ B.	출력예시)
 14	23	22	21	8
 13	12	11	10	9
 
-못했습니다... 죄송합니다...
+
 */
+
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int arrSize;
+	int arr[100][100] = { 0 };
+	cout << "배열의 크기를 입력하세요 : ";
+	cin >> arrSize;
+	cout << endl;
+	int prevSize = arrSize;
+	int posX = -1; 
+	int posY = 0;
+	int move = 1;
+	int moveCount = 1;
+	while (0 < arrSize)
+	{
+		for (int i = 0; i < arrSize; i++)
+		{
+			posX += move;
+			arr[posY][posX] = moveCount;
+			moveCount++;
+		}
+
+		arrSize--;
+
+		for (int i = 0; i < arrSize; i++)
+		{
+			posY += move;
+			arr[posY][posX] = moveCount;
+			moveCount++;
+		}
+		move *= -1;
+	}
+
+	for (int i = 0; i < prevSize; i++)
+	{
+		for (int j = 0; j < prevSize; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	
+
+
+}
+
+
+//1 2 3
+//8 9 4
+//7 6 5
+
+
